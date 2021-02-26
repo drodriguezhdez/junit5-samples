fc6fcce50660e43bec68ed32ca278672f6078129
Daniel Rodriguez Hernandez <daniel.rodriguezhernandez@datadoghq.com>
2021-02-26 10:42:23 +0100

    Reproduce issue junit5_junit4

--- junit5-multiple-engines/build.gradle.kts
+++ junit5-multiple-engines/build.gradle.kts
@@ -48,36 +48,36 @@ dependencies {
     }
 
     // Mainrunner
-    testImplementation("de.sormuras.mainrunner:de.sormuras.mainrunner.engine:2.1.4") {
+    /*testImplementation("de.sormuras.mainrunner:de.sormuras.mainrunner.engine:2.1.4") {
         because("executes Java programs as tests")
-    }
+    }*/
 
     // jqwik
-    testImplementation("net.jqwik:jqwik:1.2.0") {
+    /*testImplementation("net.jqwik:jqwik:1.2.0") {
         because("allows jqwik properties to run")
-    }
+    }*/
 
     // Spek2
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.3") {
+    /*testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.3") {
         exclude(module = "kotlin-stdlib-jdk8").because("we want to override the Kotlin version")
     }
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.3") {
         because("allows Spek2 tests to be found and executed")
-    }
+    }*/
 
     // KotlinTest
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.2.0")
+    /*testImplementation("io.kotlintest:kotlintest-runner-junit5:3.2.0")
     testRuntimeOnly("org.slf4j:slf4j-nop:1.7.25") {
         because("defaulting to no-operation (NOP) logger implementation")
-    }
+    }*/
 
     // TestNG
-    testImplementation("org.testng:testng:7.0.0") {
+    /*testImplementation("org.testng:testng:7.0.0") {
         because("allows TestNG tests to run")
     }
     testRuntimeOnly("com.github.testng-team:testng-junit5:0.0.1") {
         because("allows TestNG tests to run")
-    }
+    }*/
 }
 
 tasks {
